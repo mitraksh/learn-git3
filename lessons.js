891388a489a46d14b7299a32a8e7fab921f05b30
Mitraksh Rathod <mitrakshrathod@Mitrakshs-MacBook-Air.local>
2020-11-19 12:50:42 +0530

    Adding JS file with products

---
+++ lessons.js
@@ -0,0 +1,12 @@
+let products= [
+      { id: 0, subject: 'Chemistry', image: './images/chem.jpeg', location: 'London', price: 400, availableInventory: 5, rating: 4 },
+      { id: 1, subject: 'Math',image: './images/maths.png', location: 'Bristol', price: 300, availableInventory: 5, rating: 4 },
+      { id: 2, subject: 'Geography',image: './images/geography.jpeg', location: 'London', price: 100, availableInventory: 5, rating: 3 },
+      { id: 3, subject: 'History',image: './images/history.jpeg', location: 'London', price: 200, availableInventory: 5, rating: 5 },
+      { id: 4, subject: 'English',image: './images/english.png', location: 'Newhampshire', price: 200, availableInventory: 5, rating: 4 },
+      { id: 5, subject: 'French',image: './images/french.png', location: 'Newhampshire', price: 100, availableInventory: 5, rating: 3 },
+      { id: 6, subject: 'German',image: './images/german.png', location: 'Cambridge', price: 200, availableInventory: 5, rating: 3 },
+      { id: 7, subject: 'Political Science',image: './images/politicalscience.jpeg', location: 'Bristol', price: 300, availableInventory: 5, rating: 5 },
+      { id: 8, subject: 'Biology',image: './images/bio.png', location: 'Chelsea', price: 400, availableInventory: 5, rating: 3 },
+      { id: 9, subject: 'Physics',image: './images/physics.png', location: 'Cambridge', price: 400, availableInventory: 5, rating: 2 },
+]
